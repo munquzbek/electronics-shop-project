35c0c1959793a5e49fe3e81d4958e82c5f0bee9e
munquzbek <munchik31@gmail.com>
2023-08-31 15:13:23 -0400

    hw#1

--- src/item.py
+++ src/item.py
@@ -1,30 +1,43 @@
+import csv
+import pandas as pd
+
+
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+        return self.price * self.quantity
 
-        :return: Общая стоимость товара.
-        """
-        pass
+    def apply_discount(self) -> float:
+        self.price = self.price * Item.pay_rate
+        return self.price
 
-    def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) <= 10:
+            self.__name = value
+            return self.__name
+        else:
+            print("Exception: Длина наименования товара превышает 10 символов")
+            self.__name = value[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        csvfile = pd.read_csv("../src/items.csv")
+        print(csvfile)
+        # with open('../src/items.csv', 'a', newline='') as file:
+        #     csvfile = csv.DictReader(file)
+        #     for row in csvfile:
+        #         print(row)
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item("Смартфон", 1000, 2)
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 2000
+
+
+def test_apply_discount(item):
+    Item.pay_rate = 0.8
+    assert item.apply_discount() == 800.0
